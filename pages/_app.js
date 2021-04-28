333bb415414657571d03c47194f1a5f695a5fab1
orlasiimwe <orlanerys1@gmail.com>
2021-04-28 21:50:37 +0300

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,23 @@ function MyApp({ Component, pageProps }) {
         <title>BlogApp</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+             
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195779781-3"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'UA-195779781-3');
+              </script>
+              
+              `
+            }}
+          />
+
       </Head>
       <Component {...pageProps} />
     </>
